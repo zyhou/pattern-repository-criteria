b0858f9840ce1271a10c29be862a91d6869eeb52
Maxime <richard.maxime9@gmail.com>
2016-09-27 22:31:46 +0200

    Add project files.

---
+++ src/pattern-repository-criteria/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace pattern_repository_criteria
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+        }
+    }
+}
